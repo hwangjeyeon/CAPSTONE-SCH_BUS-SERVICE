f0bc5d8e5244259ff903895a8a9e22a081fa25c7
hwangjeyeon <kingpele9018@gmail.com>
2023-12-26 21:19:38 +0900

    Feat: 하드웨어Dto 클래스 생성

---
+++ src/main/java/graduation/busstation/dto/HardwareDto.java
@@ -0,0 +1,14 @@
+package graduation.busstation.dto;
+
+
+import lombok.Data;
+
+@Data
+public class HardwareDto {
+
+    private String name;
+    private String macAddress;
+    private String license;
+
+
+}
