7e68a9f39c55c790f1920c2f02a19622cf92053d
hwangjeyeon <kingpele9018@gmail.com>
2024-01-01 23:04:26 +0900

    fix: BusStation Entity 수정
    - Column 애노테이션 추가
    - @LastModifiedDate 애노테이션 추가로 업데이트될 때 자동 반영

--- src/main/java/graduation/busstation/entity/BusStation.java
+++ src/main/java/graduation/busstation/entity/BusStation.java
@@ -1,12 +1,14 @@
 package graduation.busstation.entity;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
@@ -19,10 +21,14 @@ public class BusStation {
     @GeneratedValue
     private Long id;
 
+    @Column(name="BUS_STATION_NAME")
     private String busStationName;
+    @Column(name="DEVICE_MAC_ADDRESS")
     private String deviceMacAddress;
+    @Column(name="STATION_STATUS")
     private String stationStatus;
+    @Column(name="DATE_TIME")
+    @LastModifiedDate
     private LocalDateTime dateTime;
-
 
 }
