9a9a5324127bd0f5ea4089d73e7ffec1380f6d3e
hwangjeyeon <kingpele9018@gmail.com>
2024-01-02 16:38:54 +0900

    Test: RenewStationInfoService 테스트

---
+++ src/test/java/graduation/busstation/service/RenewStationInfoServiceTest.java
@@ -0,0 +1,64 @@
+package graduation.busstation.service;
+
+import graduation.busstation.entity.BusStation;
+import graduation.busstation.repository.StationRepository;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+@Transactional
+class RenewStationInfoServiceTest {
+
+    @Autowired
+    RenewStationInfoService renewStationInfoService;
+
+    @Autowired
+    StationRepository stationRepository;
+
+    @BeforeEach
+    public void before(){
+        BusStation busStation = new BusStation();
+        busStation.setBusStationName("후문");
+        busStation.setDeviceMacAddress("FC-AA-14-44-4F-81");
+        stationRepository.save(busStation);
+    }
+
+
+    @Test
+    public void arrivedRenewServiceTest(){
+        BusStation findStation = stationRepository.findByBusStationName("후문").get(0);
+        LocalDateTime arrivedTime = renewStationInfoService.renewArrivedStation(findStation.getBusStationName(),
+                findStation.getDeviceMacAddress(), findStation.getId());
+
+        assertThat(findStation.getBusStationName()).isEqualTo("후문");
+        assertThat(findStation.getDeviceMacAddress()).isEqualTo("FC-AA-14-44-4F-81");
+        assertThat(findStation.getId()).isEqualTo(1L);
+        assertThat(findStation.getStationStatus()).isEqualTo("도착");
+        assertThat(findStation.getArrivedDateTime()).isEqualTo(arrivedTime);
+    }
+
+    @Test
+    public void departedRenewServiceTest(){
+        BusStation findStation = stationRepository.findByBusStationName("후문").get(0);
+        LocalDateTime departedTime = renewStationInfoService.renewDepartedStation(findStation.getBusStationName()
+                , findStation.getDeviceMacAddress(), findStation.getId());
+
+        assertThat(findStation.getBusStationName()).isEqualTo("후문");
+        assertThat(findStation.getDeviceMacAddress()).isEqualTo("FC-AA-14-44-4F-81");
+        assertThat(findStation.getId()).isEqualTo(1L);
+        assertThat(findStation.getStationStatus()).isEqualTo("출발");
+        assertThat(findStation.getDepartedDateTime()).isEqualTo(departedTime);
+    }
+
+}
