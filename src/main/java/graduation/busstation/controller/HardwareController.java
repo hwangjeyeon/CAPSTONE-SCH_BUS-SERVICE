8c6e82e15832defb5177ee8b2eb121bc7e4ed56c
hwangjeyeon <kingpele9018@gmail.com>
2024-01-02 16:14:56 +0900

    Feat: 하드웨어 컨트롤러 수정
    - 하나의 컨트롤러를 도착,출발 각각을 관리하는 컨트롤러로 분리
    - 앤드포인트 수정
    - 데이터 업데이트 서비스 코드 추가

--- src/main/java/graduation/busstation/controller/HardwareController.java
+++ src/main/java/graduation/busstation/controller/HardwareController.java
@@ -2,12 +2,12 @@ package graduation.busstation.controller;
 
 
 import graduation.busstation.dto.HardwareDto;
-import graduation.busstation.service.CarLicenseValidate;
-import graduation.busstation.service.StationValidate;
+import graduation.busstation.validate.CarLicenseValidate;
+import graduation.busstation.service.RenewStationInfoService;
+import graduation.busstation.validate.StationValidate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -21,8 +21,11 @@ public class HardwareController {
     @Autowired
     private final CarLicenseValidate carLicenseValidate;
 
-    @PostMapping("/data/receive/station")
-    public void dataReceiveStation(@RequestBody HardwareDto hardwareDto){
+    @Autowired
+    private final RenewStationInfoService renewStationInfoService;
+
+    @PostMapping("/arrived/receive/station")
+    public void arrivedDataReceiveStation(@RequestBody HardwareDto hardwareDto){
         //정류장명,mac주소가 맞는지 검증
         if(!stationValidate.validateStationInfo(hardwareDto.getName(),hardwareDto.getMacAddress())){
            log.info("접근 권한이 없습니다!");
@@ -36,8 +39,26 @@ public class HardwareController {
         }
 
         // 모두 맞으면 데이터 업데이트
+        renewStationInfoService.renewArrivedStation(hardwareDto.getName(), hardwareDto.getMacAddress(), 1L);
+    }
 
+    //TODO: 버스가 출발한 경우를 업데이트하기 위한 컨트롤러
+    @PostMapping("/departed/receive/station")
+    public void departedDataReceiveStation(@RequestBody HardwareDto hardwareDto){
+        //정류장명,mac주소가 맞는지 검증
+        if(!stationValidate.validateStationInfo(hardwareDto.getName(),hardwareDto.getMacAddress())){
+            log.info("접근 권한이 없습니다!");
+            return;
+        }
 
+        // 차량 라이센스 정보가 맞는지 검증
+        if(!carLicenseValidate.validateCarLicense(hardwareDto.getLicense())){
+            log.info("등록된 버스가 아닙니다.");
+            return;
+        }
+
+        // 모두 맞으면 데이터 업데이트
+        renewStationInfoService.renewDepartedStation(hardwareDto.getName(), hardwareDto.getMacAddress(), 1L);
     }
 
 
