0d8d8499d0bc89886ff8c424cbd9bb577166ee09
hwangjeyeon <kingpele9018@gmail.com>
2024-01-09 20:57:26 +0900

    feat: StationStatus Enum 추가

---
+++ src/main/java/graduation/busstation/Status/StationStatus.java
@@ -0,0 +1,13 @@
+package graduation.busstation.Status;
+
+
+/**
+ * ARRIVED: 도착
+ * DEPARTED: 출발
+ * TBD: (TO BE DETERMINED)의 약자로 미확정을 의미함. 즉 도착정보없음을 대체하는 단어
+ */
+public enum StationStatus {
+
+    ARRIVED, DEPARTED, TBD;
+
+}
