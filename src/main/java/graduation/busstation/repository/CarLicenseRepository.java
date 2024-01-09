24f233f5978e93264ddb8e66e8f4c5bf2738698b
hwangjeyeon <kingpele9018@gmail.com>
2024-01-09 21:01:14 +0900

    fixed untracked files

--- src/main/java/graduation/busstation/repository/CarLicenseRepository.java
+++
@@ -1,8 +0,0 @@
-package graduation.busstation.repository;
-
-import graduation.busstation.entity.CarLicense;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarLicenseRepository extends JpaRepository<CarLicense, String> {
-
-}
