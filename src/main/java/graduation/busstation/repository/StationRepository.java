c09c8c7bb0be8ce01bd5795d9f613a637aef94f0
hwangjeyeon <kingpele9018@gmail.com>
2024-01-20 22:13:56 +0900

    Feat: StationStatus 초기화 수정 쿼리 구현

--- src/main/java/graduation/busstation/repository/StationRepository.java
+++ src/main/java/graduation/busstation/repository/StationRepository.java
@@ -1,5 +1,6 @@
 package graduation.busstation.repository;
 
+import graduation.busstation.enums.StationStatus;
 import graduation.busstation.entity.BusStation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -12,4 +13,7 @@ public interface StationRepository extends JpaRepository<BusStation, Long> {
     List<BusStation> findByBusStationName(String busStationName);
     List<BusStation> findByDeviceMacAddress(String deviceMacAddress);
 
+    @Modifying(clearAutomatically = true)
+    @Query("update BusStation bs set bs.stationStatus = 'TBD'")
+    void bulkStationStatusInit();
 }
