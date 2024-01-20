c022a4101359da65f5aca78c143d8f9bd723db3d
hwangjeyeon <kingpele9018@gmail.com>
2024-01-20 22:15:53 +0900

    Test: StationStatus reset기능 동작 테스트

--- src/test/java/graduation/busstation/service/ResetStationStatusServiceTest.java
+++ src/test/java/graduation/busstation/service/ResetStationStatusServiceTest.java
@@ -1,17 +1,14 @@
 package graduation.busstation.service;
 
-import graduation.busstation.Status.StationStatus;
+import graduation.busstation.enums.StationStatus;
 import graduation.busstation.entity.BusStation;
 import graduation.busstation.repository.StationRepository;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @Transactional
 @SpringBootTest
