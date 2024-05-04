d3c4b891022e3830cbd973d911444393a720a480
hwangjeyeon <kingpele9018@gmail.com>
2024-05-05 00:23:26 +0900

    Refactor: Exception 예외 처리 로직 추가

--- src/main/java/graduation/busstation/hardware/error/ExceptionController.java
+++ src/main/java/graduation/busstation/hardware/error/ExceptionController.java
@@ -13,10 +13,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionController {
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResult> unauthorizedExceptionHandler(IllegalArgumentException e){
+    public ResponseEntity<ErrorResult> unauthorizedExceptionHandler(){
         log.error("미등록 사용자 접근");
         ErrorResult errorResult = new ErrorResult("unauthorized","접근 권한이 없습니다.");
         return new ResponseEntity<>(errorResult, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler
+    public void unknownExceptionHandler(Exception e){
+        log.error("-- 알 수 없는 예외 발생 --", e);
     }
 
 
