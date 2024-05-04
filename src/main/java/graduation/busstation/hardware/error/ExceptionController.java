17db50f64083557614a3a31c3dabcf39f08035bc
hwangjeyeon <kingpele9018@gmail.com>
2024-05-05 00:16:06 +0900

    Refactor: 깔끔한 로그 로직을 위해 stacktrace 내용 제거

--- src/main/java/graduation/busstation/hardware/error/ExceptionController.java
+++ src/main/java/graduation/busstation/hardware/error/ExceptionController.java
@@ -1,6 +1,7 @@
 package graduation.busstation.hardware.error;
 
 
+import graduation.busstation.hardware.util.ClientIpUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,7 @@ public class ExceptionController {
 
     @ExceptionHandler
     public ResponseEntity<ErrorResult> unauthorizedExceptionHandler(IllegalArgumentException e){
-        log.error("미등록 사용자 접근 ",e);
+        log.error("미등록 사용자 접근");
         ErrorResult errorResult = new ErrorResult("unauthorized","접근 권한이 없습니다.");
         return new ResponseEntity<>(errorResult, HttpStatus.FORBIDDEN);
     }
