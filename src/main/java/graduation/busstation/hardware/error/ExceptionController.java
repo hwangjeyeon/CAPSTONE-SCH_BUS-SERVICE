ea70bd983b4c6ce94a9d6def3b033ced3c30b5eb
hwangjeyeon <kingpele9018@gmail.com>
2024-05-06 00:28:31 +0900

    Feat: 예외처리 항목 추가 구현

--- src/main/java/graduation/busstation/hardware/error/ExceptionController.java
+++ src/main/java/graduation/busstation/hardware/error/ExceptionController.java
@@ -1,10 +1,11 @@
 package graduation.busstation.hardware.error;
 
 
-import graduation.busstation.hardware.util.ClientIpUtil;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,14 +13,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionController {
 
-    @ExceptionHandler
-    public ResponseEntity<ErrorResult> unauthorizedExceptionHandler(){
-        log.error("미등록 사용자 접근");
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResult> unauthorizedExceptionHandler(IllegalArgumentException e){
+        log.error("미등록 사용자 접근, 검증 실패 이유 = {}", e.getMessage());
         ErrorResult errorResult = new ErrorResult("unauthorized","접근 권한이 없습니다.");
         return new ResponseEntity<>(errorResult, HttpStatus.FORBIDDEN);
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public void parsingExceptionHandler(){
+        log.error("잘못된 JSON 형식 전송");
+    }
+
+    @ExceptionHandler(Exception.class)
     public void unknownExceptionHandler(Exception e){
         log.error("-- 알 수 없는 예외 발생 --", e);
     }
